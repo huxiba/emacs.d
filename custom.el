506f625f8c90e6a9b0a20b64e167eb760821bfb2
Yao Yong <yaoy@stpass.com>
2023-07-21 16:18:18 +0800

    feat(CSS) 增加CSS

--- custom.el
+++ custom.el
@@ -5,8 +5,9 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(sanityinc-tomorrow-bright))
  '(package-selected-packages
-   '(use-package envrc uptimes shfmt dotenv-mode osx-location htmlize lua-mode gnuplot sudo-edit flycheck-ledger ledger-mode dash-at-point origami regex-tool info-colors flycheck-clojure cider elein cljsbuild-mode clojure-mode slime cask-mode flycheck-relint cl-libify package-lint-flymake highlight-quoted macrostep aggressive-indent immortal-scratch auto-compile ipretty elisp-slime-nav paredit nginx-mode nixos-options nix-buffer nix-sandbox nixpkgs-fmt nix-mode terraform-mode docker-compose-mode dockerfile-mode docker yaml-mode flycheck-rust rust-mode flycheck-nim nim-mode j-mode dune-format dune tuareg sqlformat projectile-rails yard-mode bundler yari robe ruby-compilation inf-ruby rspec-mode ruby-hash-syntax psci psc-ide purescript-mode elm-test-runner elm-mode dhall-mode haskell-mode reformatter toml-mode pip-requirements restclient httprepl haml-mode css-eldoc skewer-less sass-mode rainbow-mode tagedit org-pomodoro writeroom-mode org-cliplink grab-mac-link smarty-mode php-mode add-node-modules-path skewer-mode js-comint coffee-mode prettier-js typescript-mode js2-mode json-mode erlang csv-mode markdown-mode textile-mode crontab-mode alert ibuffer-projectile github-review forge github-clone bug-reference-github yagist git-commit magit-todos magit git-link git-timemachine git-modes git-blamed vc-darcs whitespace-cleanup-mode which-key highlight-escape-sequences whole-line-or-region move-dup page-break-lines multiple-cursors avy browse-kill-ring symbol-overlay rainbow-delimiters mode-line-bell vlf list-unicode-display unfill mmm-mode session windswap switch-window corfu orderless marginalia consult-flycheck embark-consult projectile consult embark vertico consult-eglot eglot flymake-flycheck flymake ibuffer-vc wgrep anzu diff-hl diredfl disable-mouse default-text-scale ns-auto-titlebar dimmer color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized command-log-mode scratch diminish exec-path-from-shell gnu-elpa-keyring-update fullframe seq web-mode company-lsp undo-tree lsp-ivy lsp-ui lsp-mode pipenv elpy company-box))
- '(session-use-package t nil (session)))
+   '(use-package envrc uptimes shfmt dotenv-mode osx-location htmlize lua-mode gnuplot sudo-edit flycheck-ledger ledger-mode dash-at-point origami regex-tool info-colors flycheck-clojure cider elein cljsbuild-mode clojure-mode slime cask-mode flycheck-relint cl-libify package-lint-flymake highlight-quoted macrostep aggressive-indent immortal-scratch auto-compile ipretty elisp-slime-nav paredit nginx-mode nixos-options nix-buffer nix-sandbox nixpkgs-fmt nix-mode terraform-mode docker-compose-mode dockerfile-mode docker yaml-mode flycheck-rust rust-mode flycheck-nim nim-mode j-mode dune-format dune tuareg sqlformat projectile-rails yard-mode bundler yari robe ruby-compilation inf-ruby rspec-mode ruby-hash-syntax psci psc-ide purescript-mode elm-test-runner elm-mode dhall-mode haskell-mode reformatter toml-mode pip-requirements restclient httprepl haml-mode css-eldoc skewer-less sass-mode rainbow-mode tagedit org-pomodoro writeroom-mode org-cliplink grab-mac-link smarty-mode php-mode add-node-modules-path skewer-mode js-comint coffee-mode prettier-js typescript-mode js2-mode json-mode erlang csv-mode markdown-mode textile-mode crontab-mode alert ibuffer-projectile github-review forge github-clone bug-reference-github yagist git-commit magit-todos magit git-link git-timemachine git-modes git-blamed vc-darcs whitespace-cleanup-mode which-key highlight-escape-sequences whole-line-or-region move-dup page-break-lines multiple-cursors avy browse-kill-ring symbol-overlay rainbow-delimiters mode-line-bell vlf list-unicode-display unfill mmm-mode session windswap switch-window corfu orderless marginalia consult-flycheck embark-consult projectile consult embark vertico consult-eglot eglot flymake-flycheck flymake ibuffer-vc wgrep anzu diff-hl diredfl disable-mouse default-text-scale ns-auto-titlebar dimmer color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized command-log-mode scratch diminish exec-path-from-shell gnu-elpa-keyring-update fullframe seq company-bootstrap web-mode company-lsp undo-tree lsp-ivy lsp-ui lsp-mode pipenv elpy company-box))
+ '(session-use-package t nil (session))
+ '(warning-suppress-types '((use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -131,17 +132,16 @@
   ;; (setq consult-project-function nil)
   )
 
-
 (use-package lsp-mode
   :ensure t
   :commands lsp
   :hook ((js-mode css-mode scss-mode web-mode) . lsp)
   :init
   (setq lsp-prefer-flymake nil)
+  :config
   (require 'lsp-clients))
 
 (use-package lsp-ui :ensure t :commands lsp-ui-mode)
-(use-package company-lsp :ensure t :commands company-lsp)
 
 (use-package web-mode
   :ensure t
@@ -150,7 +150,28 @@
 (use-package company
   :ensure t
   :init
+                                        ;(require 'company-bootstrap)          ; Import required backends
+  (require 'company-web)
+  (add-to-list 'company-backends '(company-tern company-web-html company-yasnippet company-files company-etags))
   (global-company-mode)
   (setq company-idle-delay 0.2)
   (setq company-minimum-prefix-length 1)
-  (setq company-show-numbers nil))
+  (setq company-show-numbers t)
+  (setq company-tooltip-align-annotations t))
+
+(with-eval-after-load 'company-etags '(progn (add-to-list 'company-etags-modes 'web-mode)))
+(setq company-etags-everywhere '(html-mode web-mode nxml-mode))
+
+(use-package company-lsp :ensure t :commands company-lsp)
+
+(use-package elpy
+  :ensure t
+  :defer t
+  :init
+  (advice-add 'python-mode :before 'elpy-enable))
+
+(use-package pipenv
+  :ensure t
+  :hook (python-mode . pipenv-mode)
+  :init
+  )
